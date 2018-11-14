ea6436bd85aeb866c1954a793944ee67d3ed03d3
Aluno <aluno@edu.senac.br>
2018-11-14 21:42:30 -0200

    enviando index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html >
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+	<h2>
+		
+	<?php 
+	$nome = "Valeria";
+
+
+
+	echo $nome;
+
+	 ?>
+	</h2>
+
+</body>
+</html>
